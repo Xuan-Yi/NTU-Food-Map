74e01230251535f646ca63f273ba86d828c88547
Xuan-Yi <redpig815@gmail.com>
2023-01-18 12:44:35 +0800

    add Slimlix1101's map and timetable

--- lib/containers/home_page.dart
+++ lib/containers/home_page.dart
@@ -1,5 +1,6 @@
-import 'package:firebase_auth/firebase_auth.dart'; // Fire base Auth
 import 'package:flutter/material.dart';
+import 'map_page.dart';
+import 'timetable_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -9,44 +10,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   @override
-  Widget build(BuildContext context) {
-    final user = FirebaseAuth.instance.currentUser!;
-
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-        backgroundColor: Colors.green,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'Signed In as',
-              style: TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 8),
-            Text(
-              user.email!,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
-            ),
-            const SizedBox(height: 40),
-            ElevatedButton.icon(
-              style: ElevatedButton.styleFrom(
-                minimumSize: const Size.fromHeight(50),
-                backgroundColor: Colors.green,
-              ),
-              icon: const Icon(Icons.arrow_back, size: 32),
-              label: const Text(
-                'Sign Out',
-                style: TextStyle(fontSize: 24),
-              ),
-              onPressed: () => FirebaseAuth.instance.signOut(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/map',
+        routes: {
+          '/map': (context) => const MapPage(),
+          '/timetable': (context) => const TimetablePage(),
+        },
+      );
 }
